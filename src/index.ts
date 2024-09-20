50706f5d9630009a7bddbb6e8536acd8645ffd20
Mohammed Shibil PV <muhammedshibilpv77@gmail.com>
2024-09-20 21:33:30 +0530

    body size inc

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ const corsOptions = {
 
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true ,limit: '10mb'}));
 app.use(morgan('dev'));
 app.use('/', routes);
 
