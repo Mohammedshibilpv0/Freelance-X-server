0b0ae28e44bf8adefa31f8566c4be0c82dfef731
Mohammed Shibil PV <muhammedshibilpv77@gmail.com>
2024-09-20 18:26:30 +0530

    firebase

--- src/infrastructure/middleware/authMiddleware.ts
+++ src/infrastructure/middleware/authMiddleware.ts
@@ -11,8 +11,6 @@ const authMiddleware = (requireAdmin: boolean = false) => {
 
   return async (req: CustomRequest, res: Response, next: NextFunction) => {
     let  token = req.cookies.accessToken; 
-    console.log(process.env.COOKIESECURE)
-    console.log(token)
     if (!token) {
       return res.status(401).json({ error: 'No token provided' });
     }
