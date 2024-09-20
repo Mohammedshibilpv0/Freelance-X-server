5885586fd2c7e94bfbed5e1f1cf5e07e5323ea1b
Mohammed Shibil PV <muhammedshibilpv77@gmail.com>
2024-09-20 16:55:50 +0530

    firebase

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,9 +3,10 @@ import admin from 'firebase-admin';
 import * as serviceAccount from './fir-87d6a-firebase-adminsdk-j32ju-f886c86b6b.json';
 import { FIREBASEBUCKET } from '../config/env';
 
+console.log('FIREBASEBUCKET',FIREBASEBUCKET)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: FIREBASEBUCKET,
+  storageBucket: 'gs://fir-87d6a.appspot.com/',
 });
 
 const bucket = admin.storage().bucket();
